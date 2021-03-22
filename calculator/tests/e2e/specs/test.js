d3b4c0d45d2a1c5601a6dbcdaf706e4f6da71a85
Mark Burns <markburns@gmail.com>
2021-03-22 23:13:05 +0000

    e2e tests one and two complete

--- calculator/tests/e2e/specs/test.js
+++ calculator/tests/e2e/specs/test.js
@@ -2,11 +2,51 @@
 
 describe('calculator', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/', () => {
+
+    })
   });
 
   it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('number buttons update running total display', () => {
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('#number7').click();
+    cy.get('.display').should('contain', '457')
+  })
+
+  it('arithmetic operations update display with result', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_add').click()
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click()
+    cy.get('.display').should('contain', '6')
+  })
+
+  xit('multiple operations chain together', () => {
+
+  })
+
+  xit('', () => {
+
+  })
+
+  xit('', () => {
+
+  })
 })
+
+
+// Do the number buttons update the display of the running total?
+
+// Do the arithmetical operations update the display with the result of the operation?
+
+// Can multiple operations be chained together?
+
+// Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
+// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
